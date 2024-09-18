6cdabcd53db090e84103193bb2a5785bc9b09343
Peter Gebri <peter.gebri@trendizz.com>
2024-09-18 19:06:58 +0200

    files for us

--- room/room.go
+++ room/room.go
@@ -1,1 +1,9 @@
 package room
+
+// Room működése:
+// Minden oldal egy Ajtó vagy egy fal lehet kizárólag.
+// Amikor meghívjuk a függvényt, akkor random dönse el, hogy melyik oldal tartalmaz ajtót és melyik oldal falat.
+// Így csak és kizárólag a visszaadott értékek door vagy wall lehetnek.
+func Room() (right string, left string, front string, rear string) {
+	return "hibás", "hibás", "hibás", "hibás"
+}
--- tunnel/tunnel.go
+++ tunnel/tunnel.go
@@ -1,1 +1,11 @@
 package tunnel
+
+// RandTunnel működése:
+// Vissza kell tudja adni, hogy mi van előre jobbra és balra
+// Kell tudjon választani, hogy az adott irányba mi van.
+// Lehet hogy újabb Tunnel van és lehet, hogy Room van.
+// Front a right és a left csak és kizárólag Room vagy Tunnel értéket vehet fel.
+// És minden hívásnál random dőljön el, hogy melyik irányba mit adunk vissza.
+func RandTunnel() (string, string, string) {
+	return "hibás", "hibás", "hibás"
+}
