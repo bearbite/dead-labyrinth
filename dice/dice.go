bddcf5464a8b36cd731ce66e2f0812c6cfb43998
hackpink <fruzsina.gebri@trendizz.com>
2024-09-20 17:01:50 +0200

    elkészítettem a dobókockát

--- dice/dice.go
+++ dice/dice.go
@@ -1,6 +1,11 @@
 package dice
 
+import "math/rand"
+
 // Roll visszaad két véletlenszerű számot 1 és 6 között
 func Roll() (int, int) {
-	return 0, 0
+	roll1 := rand.Intn(6) + 1
+	roll2 := rand.Intn(6) + 1
+	return roll1, roll2
+
 }
--- dice/dice_test.go
+++ dice/dice_test.go
@@ -1,10 +1,16 @@
 package dice
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRoll(t *testing.T) {
 
 	dice1, dice2 := Roll()
+
+	fmt.Println(dice1)
+	fmt.Println(dice2)
 
 	if dice1 < 1 || dice1 > 6 {
 		t.Error("A dobokocka 1 elvárt értéke 1 és 6 között van, de a kapott érték:", dice1)
